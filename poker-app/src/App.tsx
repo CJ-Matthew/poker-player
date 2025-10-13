5cc6d0fd07134fbd6485f4203d7080cd8bca9a0a
Caleb Matthew <caleb.john.matthew@gmail.com>
2025-10-13 17:10:51 +1300

    import error

--- poker-app/src/App.tsx
+++ poker-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { WelcomeScreen } from './components/WelcomeScreen';
 import { LobbyScreen } from './components/LobbyScreen';
 import { TableScreen } from './components/TableScreen';
